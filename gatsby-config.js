680e3d65288ad5ea7e52b1f873384a114e44216f
Jameson Steiner <jameson.steiner@gmail.com>
2021-03-04 22:27:37 +0100

    Add gatsby-plugin-ts for automatic GraphQL query type generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,21 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-ts`,
+      options: {
+        tsLoader: {
+          logLevel: 'warn',
+        },
+        forkTsCheckerPlugin: {
+          eslint: true,
+        },
+        fileName: `types/graphql-types.ts`,
+        codegen: true,
+        codegenDelay: 250,
+        alwaysCheck: false,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
